2d1db036236ee718be2b7560214da77b4f350e74
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-02-19 00:57:18 +0300

    card_adventure_init

---
+++ include/card_adventure.hpp
@@ -0,0 +1,23 @@
+#ifndef CARD_ADVENTURE_HPP_
+#define CARD_ADVENTURE_HPP_
+
+namespace runebound::cards {
+struct CardAdventure {
+
+  unsigned int height();
+  unsigned int width();
+  CardAdventure(const CardAdventure &) = delete;
+  CardAdventure(CardAdventure &&) = delete;
+  CardAdventure &operator=(const CardAdventure &) = delete;
+  CardAdventure &operator=(CardAdventure &&) = delete;
+
+protected:
+  CardAdventure() = default;
+private:
+  const unsigned int m_height;
+  const double m_ratio;
+};
+
+} // namespace runebound::cards
+
+#endif  // CARD_ADVENTURE_HPP_
---
+++ src/card_adventure.cpp
@@ -0,0 +1,1 @@
+#include "card_adventure.hpp"
