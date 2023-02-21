1abda9af0a8f61b67d586998ad150a1ce409b5f6
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-02-21 13:48:12 +0300

    tokens_for_cells

--- include/field_cell.hpp
+++ include/field_cell.hpp
@@ -1,11 +1,14 @@
 #ifndef FIELD_CELL_HPP_
 #define FIELD_CELL_HPP_
 
+#include <memory>
 #include <utility>
 #include <vector>
-#include <memory>
+#include "runebound_fwd.hpp"
 
 namespace runebound::field {
+
+const int COUNT_NEIGHBOUR = 6;
 
 enum class TypeCell { Joker, Water, Forest, Mountains, Hills, Plain };
 
@@ -22,15 +25,36 @@ struct FieldCell {
 public:
     FieldCell(TypeCell type_cell) : m_type_cell(type_cell) {
         m_neighbours.resize(COUNT_NEIGHBOUR, nullptr);
+        m_token = runebound::AdventureType::Nothing;
+        m_side_token = runebound::Side::Front;
     }
 
-    void make_neighbour(std::shared_ptr <FieldCell> neighbour, Neighbour direction) {
+    void
+    make_neighbour(std::shared_ptr<FieldCell> neighbour, Neighbour direction) {
         m_neighbours[static_cast<int>(direction)] = std::move(neighbour);
     }
 
+    void make_token(runebound::AdventureType token) {
+        m_token = token;
+    }
+
+    runebound::AdventureType get_token() {
+        return m_token;
+    }
+
+    void reverse_token() {
+        m_side_token =
+            static_cast<runebound::Side>(static_cast<int>(m_side_token) ^ 1);
+    }
+
+    runebound::Side get_side_token() {
+        return m_side_token;
+    }
+
 private:
-    const int COUNT_NEIGHBOUR = 6;
     const TypeCell m_type_cell;
+    runebound::AdventureType m_token;
+    runebound::Side m_side_token;
     std::vector<std::shared_ptr<FieldCell>> m_neighbours;
 };
 
---
+++ include/runebound_fwd.hpp
@@ -0,0 +1,11 @@
+#ifndef RUNEBOUND_FWD_HPP_
+#define RUNEBOUND_FWD_HPP_
+
+namespace runebound {
+enum class AdventureType { Meeting, Research, Fight, Nothing };
+
+enum class Side { Front, Back };
+
+}  // namespace runebound
+
+#endif  // RUNEBOUND_FWD_HPP_
