4238a77b1ad0674e398f200a5dc32f5a5ef8b29b
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-02-23 23:07:11 +0300

    character raw version

---
+++ src/character.cpp
@@ -0,0 +1,17 @@
+#include "character.hpp"
+
+namespace runebound {
+namespace character {
+std::unique_ptr <::runebound::cards::CardAdventure> Character::pop_card(::runebound::cards::CardAdventure *card) {
+    for (std::size_t i = 0; i < m_cards.size(); ++i) {
+        if (m_cards[i].get() == card) {
+            std::swap(m_cards[i], m_cards.back());
+            break;
+        }
+    }
+    auto desired_card = std::move(m_cards.back());
+    m_cards.pop_back();
+    return desired_card;
+}
+} // namespace character
+} // namespace runebound
