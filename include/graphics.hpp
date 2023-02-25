7b5647537c010ace9f6b2d6c427d410c3782c175
Artem Batygin <aa.batygin@gmail.com>
2023-02-25 23:05:05 +0300

    ready to merge

---
+++ include/graphics.hpp
@@ -0,0 +1,5 @@
+#ifndef RUBEBOUND_GRAPHICS_HPP_
+#define RUBEBOUND_GRAPHICS_HPP_
+namespace runebound::graphics {
+} // namespace runebound::graphics
+#endif // RUBEBOUND_GRAPHICS_HPP_
---
+++ src/graphics.cpp
@@ -0,0 +1,6 @@
+#include "graphics.hpp"
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, World!\n";
+}
