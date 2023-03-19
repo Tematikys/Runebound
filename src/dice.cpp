2d4badc3ced69a54d78371af0bf4f802d04ffed6
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-03-19 17:50:14 +0300

    get combination dice

--- src/dice.cpp
+++ src/dice.cpp
@@ -1,4 +1,5 @@
 #include "dice.hpp"
+#include <vector>
 
 namespace runebound {
 namespace dice {
@@ -6,6 +7,15 @@ namespace dice {
 HandDice throw_dice() {
     int result = ::runebound::rng() % 6;
     return static_cast<HandDice>(result);
+}
+
+std::vector<HandDice> get_combination_of_dice(unsigned int count_throws) {
+    std::vector<HandDice> result_of_throws(count_throws);
+    for (unsigned int number_dice = 0; number_dice < count_throws;
+         ++number_dice) {
+        result_of_throws[number_dice] = throw_dice();
+    }
+    return result_of_throws;
 }
 }  // namespace dice
 }  // namespace runebound
