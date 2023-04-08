ed864a3cf2de1e25c4dbf028b9dbd2a536027015
unknown <bnmmnb456654@inbox.ru>
2023-04-08 23:28:00 +0300

    server and client woth some json

--- src/client.cpp
+++ src/client.cpp
@@ -10,8 +10,8 @@ using json = nlohmann::json;
 
 class Client {
 public:
-    Client(boost::asio::io_context &io_context, const std::string &host, int port)
-            : socket_(io_context) {
+    Client(boost::asio::io_context &io_context, const std::string &host, int port, std::string user_name)
+            : socket_(io_context), m_user_name(user_name) {
         tcp::resolver resolver(io_context);
         auto endpoints = tcp::endpoint(boost::asio::ip::address::from_string(host), port);
         socket_.async_connect(endpoints,
@@ -29,7 +29,11 @@ public:
                                 [this](boost::system::error_code ec, std::size_t length) {
                                     if (!ec) {
                                         std::string_view stringView(read_buffer_, length);
-                                        std::cout << stringView << std::endl;
+                                        json answer = json::parse(stringView);
+
+                                        if (answer["change type"] == "game names") {
+                                            game_names = answer["game names"];
+                                        }
                                         do_read();
                                     } else {
                                         std::cerr << "Read failed: " << ec.message() << std::endl;
@@ -42,7 +46,6 @@ public:
         socket_.async_write_some(boost::asio::buffer(str),
                                  [this, str](boost::system::error_code ec, std::size_t length) {
                                      if (!ec) {
-                                         std::cout << "Sent: " << str << std::endl;
                                      } else {
                                          std::cerr << "Write failed: " << ec.message() << std::endl;
                                          socket_.close();
@@ -50,49 +53,88 @@ public:
                                  });
     }
 
+    void reverse_token(int x, int y) {
+        json data;
+        data["action type"] = "reverse token";
+        data["x"] = x;
+        data["y"] = y;
+        do_write(data.dump());
+    }
+
+    void add_game(std::string game_name) {
+        json data;
+        data["action type"] = "add game";
+        data["game name"] = game_name;
+        do_write(data.dump());
+    }
+
+    void join_game(std::string game_name) {
+        json data;
+        data["action type"] = "join game";
+        data["game name"] = game_name;
+        data["user name"] = m_user_name;
+        do_write(data.dump());
+    }
+
+
+public:
+    std::string m_user_name;
+    std::vector<std::string> game_names;
 private:
     tcp::socket socket_;
     char read_buffer_[1024];
 };
 
+
 int main() {
+    std::string user_name;
+    std::cout<<"Write your name:\n";
+    std::cin>>user_name;
+
     boost::asio::io_context io_context;
     auto work_guard = boost::asio::make_work_guard(io_context);
-    Client client(io_context, "127.0.0.1", 4444);
-    while (!io_context.stopped()) {
-        try {
-            json data;
-            std::string str;
-            std::cin >> str;
-            std::cout<<str;
-            if (str == "reverse_token") {
-                std::cout<<"ok";
-                int x, y;
-                std::cin >> x >> y;
+    Client client(io_context, "127.0.0.1", 4444, user_name);
+    try {
+        //test commands begin
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        io_context.poll();
 
-                data["action type"] = "reverse token";
-                data["x"] = x;
-                data["y"] = y;
+        std::cout<<"List of games:\n";
+        for (auto e : client.game_names){
+            std::cout<<e<<'\n';
+        }
+        std::cout<<'\n';
 
-                client.do_write(data.dump());
-            }
-//            if (str == "add game") {
-//                data["add game"] = "reverse token";
-//            }
-//            if (str == "get games") {
-//
-//            }
-//            if (str == "connect") {
-//
-//            }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        io_context.poll();
+
+        client.add_game("My game");
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        io_context.poll();
 
 
-            std::this_thread::sleep_for(std::chrono::milliseconds (100));
+
+        std::cout<<"List of games:\n";
+        for (auto e : client.game_names){
+            std::cout<<e<<'\n';
+        }
+        std::cout<<'\n';
+        client.join_game("My game");
+        // test commands end
+
+        while (!io_context.stopped()) {
+
+            //commands
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
             io_context.poll();
         }
-        catch (std::exception &e) {
-            std::cerr << "Exception: " << e.what() << std::endl;
-        }
     }
+    catch (std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+
     return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -15,99 +15,112 @@ using json = nlohmann::json;
 
 class Session;
 
-runebound::game::Game test_game;
-
 std::map<std::string, runebound::game::Game> games;
-std::set<Session*> sessions;
+std::map<std::string, std::set<std::string>> game_users;
+std::map<std::string, ::runebound::character::Character *> user_character;
+std::vector<std::string> game_names;
+std::set<Session *> sessions;
 
-class Session : public std::enable_shared_from_this<Session>
-{
+class Session : public std::enable_shared_from_this<Session> {
 public:
     Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
-    void start()
-    {
-        std::cout<<"Some connected\n";
-        sessions.insert(this);
-//        write("Hello from server\n");
-        do_read();
+    void send_games();
 
+    void start() {
+        std::cout << "Some connected\n";
+        sessions.insert(this);
+        do_read();
+        send_games();
     }
 
-    void write(const std::string& message)
-    {
+    void write(const std::string &message) {
         auto self(shared_from_this());
         boost::asio::async_write(socket_, boost::asio::buffer(message),
-                                 [this, self, message](boost::system::error_code ec, std::size_t length)
-                                 {
-                                     if (!ec)
-                                     {
+                                 [this, self, message](boost::system::error_code ec, std::size_t length) {
+                                     if (!ec) {
                                          std::cout << "Sent:{ \n " << message << "\n}\n";
-                                     }
-                                     else
-                                     {
+                                     } else {
                                          sessions.erase(this);
                                          std::cout << "Disconnected" << std::endl;
                                      }
                                  });
     }
 
-    tcp::socket& socket() { return socket_; }
+    tcp::socket &socket() { return socket_; }
 
 private:
-    void do_read()
-    {
+    void do_read() {
         auto self(shared_from_this());
         socket_.async_read_some(boost::asio::buffer(data_, 1024),
-                                [this, self](boost::system::error_code ec,  std::size_t length)
-                                {
-                                    if (!ec)
-                                    {
-                                        std::string message(data_, length);
+                                [this, self](boost::system::error_code ec, std::size_t length) {
+                                    if (!ec) {
+                                        std::string_view message(data_, length);
                                         std::cout << "Received: " << message;
-                                        write("Message getted\n");
-                                        json data=json::parse(message);
-                                        std::cout<<"OK\n";
-                                        if (data["action type"]=="reverse token") {
-                                            int row=data["x"], column=data["y"];
-                                            std::cout<<row<<' '<<column<<'\n';
-                                            std::cout<<static_cast<int>(test_game.m_map.get_cell_map(runebound::Point(row, column)).get_side_token())<<'\n';
-                                            test_game.reverse_token(data["x"],data["y"]);
-                                            std::cout<<static_cast<int>(test_game.m_map.get_cell_map(runebound::Point(row, column)).get_side_token())<<'\n';
+                                        json data = json::parse(message);
+                                        if (data["action type"] == "reverse token") {
+                                            std::string game_name = data["game name"];
+                                            auto game = games[game_name];
+                                            int row = data["x"], column = data["y"];
+                                            std::cout << row << ' ' << column << '\n';
+                                            std::cout << static_cast<int>(game.m_map.get_cell_map(
+                                                    runebound::Point(row, column)).get_side_token()) << '\n';
+                                            game.reverse_token(data["x"], data["y"]);
+                                            std::cout << static_cast<int>(game.m_map.get_cell_map(
+                                                    runebound::Point(row, column)).get_side_token()) << '\n';
 
                                         }
+                                        if (data["action type"] == "add game") {
+                                            std::string game_name = data["game name"];
+                                            game_names.push_back(game_name);
+                                            games[game_name] = runebound::game::Game();
+                                            for (auto session: sessions) {
+                                                session->send_games();
+                                            }
+                                        }
+                                        if (data["action type"] == "join game") {
+                                            std::string game_name = data["game name"];
+                                            std::string user_name = data["user name"];
+
+                                            game_ = &games[game_name];
+                                            game_users[game_name].insert(user_name);
+                                            user_character[user_name] = game_->make_character(0, 0, {0, 0}, 0, 0,
+                                                                                              "zero");
+                                        }
                                         do_read();
-                                    }
-                                    else
-                                    {
+                                    } else {
                                         sessions.erase(this);
                                         std::cout << "Disconnected" << std::endl;
                                     }
                                 });
     }
-    runebound::game::Game* game_=nullptr;
+
+
+    runebound::game::Game *game_ = nullptr;
     tcp::socket socket_;
     char data_[1024];
 };
 
-class Server
-{
+void Session::send_games() {
+    json answer;
+    answer["change type"] = "game names";
+    answer["game names"] = game_names;
+    write(answer.dump());
+}
+
+class Server {
 public:
-    Server(boost::asio::io_context& io_context, short port)
-            : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
-    {
-        std::cout<<"Server started\n";
+    Server(boost::asio::io_context &io_context, short port)
+            : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+        std::cout << "Server started\n";
         do_accept();
     }
 
 private:
-    void do_accept()
-    {
+    void do_accept() {
         acceptor_.async_accept(
-                [this](boost::system::error_code ec, tcp::socket socket)
-                {
-                    if (!ec)
-                    {
+                [this](boost::system::error_code ec, tcp::socket socket) {
+                    if (!ec) {
                         std::make_shared<Session>(std::move(socket))->start();
                     }
 
@@ -118,16 +131,13 @@ private:
     tcp::acceptor acceptor_;
 };
 
-int main()
-{
-    try
-    {
+int main() {
+    try {
         boost::asio::io_context io_context;
         Server server(io_context, 4444);
         io_context.run();
     }
-    catch (std::exception& e)
-    {
+    catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
 
