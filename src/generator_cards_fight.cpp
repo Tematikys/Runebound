575e71a5bb088b18cf88eab1b23191bdc6cce23e
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-04-14 18:23:08 +0300

    card fight start

--- src/generator_cards_fight.cpp
+++ src/generator_cards_fight.cpp
@@ -14,65 +14,20 @@ void write_to_file(
 }  // namespace
 
 void generate_cards_fight() {
-    write_to_file(
-        cards::CardFight(6, "Witch", 2),
-        "../data/json/cards/cards_fight/witch.json"
-    );
-    write_to_file(
-        cards::CardFight(7, "Terrifying", 3),
-        "../data/json/cards/cards_fight/terrifying.json"
-    );
-    write_to_file(
-        cards::CardFight(7, "Ettin", 3),
-        "../data/json/cards/cards_fight/ettin.json"
-    );
-    write_to_file(
-        cards::CardFight(8, "Ogre", 3),
-        "../data/json/cards/cards_fight/ogre.json"
-    );
-    write_to_file(
-        cards::CardFight(5, "Deep Elf", 2),
-        "../data/json/cards/cards_fight/deep_elf.json"
-    );
-    write_to_file(
-        cards::CardFight(3, "A horde of the Dead", 2),
-        "../data/json/cards/cards_fight/horde_dead.json"
-    );
-    write_to_file(
-        cards::CardFight(5, "The Rebels", 1),
-        "../data/json/cards/cards_fight/the_rebels.json"
-    );
-    write_to_file(
-        cards::CardFight(6, "Death Knight", 3),
-        "../data/json/cards/cards_fight/death_knight.json"
-    );
-    write_to_file(
-        cards::CardFight(6, "The Rogue Orc", 2),
-        "../data/json/cards/cards_fight/the_rogue_ork.json"
-    );
-    write_to_file(
-        cards::CardFight(5, "Razorwing", 2),
-        "../data/json/cards/cards_fight/razorwing.json"
-    );
-    write_to_file(
-        cards::CardFight(6, "The Beastman", 2),
-        "../data/json/cards/cards_fight/the_beastman.json"
-    );
-    write_to_file(
-        cards::CardFight(5, "The Great Dragon", 2),
-        "../data/json/cards/cards_fight/the_great_dragon.json"
-    );
-    write_to_file(
-        cards::CardFight(6, "Moon Cat", 2),
-        "../data/json/cards/cards_fight/moon_cat.json"
-    );
-    write_to_file(
-        cards::CardFight(6, "Dragon Hybrid", 2),
-        "../data/json/cards/cards_fight/dragon_hybrid.json"
-    );
-    write_to_file(
-        cards::CardFight(8, "The Restless Serpent", 3),
-        "../data/json/cards/cards_fight/the_restless_serpent.json"
-    );
+    write_to_file(cards::CardFight(6, "Witch", 2), "../data/json/cards/cards_fight/witch.json");
+    write_to_file(cards::CardFight(7, "Terrifying", 3), "../data/json/cards/cards_fight/terrifying.json");
+    write_to_file(cards::CardFight(7, "Ettin", 3), "../data/json/cards/cards_fight/ettin.json");
+    write_to_file(cards::CardFight(8, "Ogre", 3), "../data/json/cards/cards_fight/ogre.json");
+    write_to_file(cards::CardFight(5, "Deep Elf", 2), "../data/json/cards/cards_fight/deep_elf.json");
+    write_to_file(cards::CardFight(3, "A horde of the Dead", 2), "../data/json/cards/cards_fight/horde_dead.json");
+    write_to_file(cards::CardFight(5, "The Rebels", 1), "../data/json/cards/cards_fight/the_rebels.json");
+    write_to_file(cards::CardFight(6, "Death Knight", 3), "../data/json/cards/cards_fight/death_knight.json");
+    write_to_file(cards::CardFight(6, "The Rogue Orc", 2), "../data/json/cards/cards_fight/the_rogue_ork.json");
+    write_to_file(cards::CardFight(5, "Razorwing", 2), "../data/json/cards/cards_fight/razorwing.json");
+    write_to_file(cards::CardFight(6, "The Beastman", 2), "../data/json/cards/cards_fight/the_beastman.json");
+    write_to_file(cards::CardFight(5, "The Great Dragon", 2), "../data/json/cards/cards_fight/the_great_dragon.json");
+    write_to_file(cards::CardFight(6, "Moon Cat", 2), "../data/json/cards/cards_fight/moon_cat.json");
+    write_to_file(cards::CardFight(6, "Dragon Hybrid", 2), "../data/json/cards/cards_fight/dragon_hybrid.json");
+    write_to_file(cards::CardFight(8, "The Restless Serpent", 3), "../data/json/cards/cards_fight/the_restless_serpent.json");
 }
-}  // namespace runebound::generator
+}
