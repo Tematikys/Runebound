4545f204a7691544c9e8df5844f281f1cd9849dc
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-04-15 19:54:39 +0300

    cmake and main changes

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "game.hpp"
-#include "runebound_fwd.hpp"
-
-int main() {
-    ::runebound::generator::generate_characters();
-    ::runebound::generator::generate_cards_fight();
-    ::runebound::game::Game game;
-}
