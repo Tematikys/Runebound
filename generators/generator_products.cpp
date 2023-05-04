cfcc9a784b660ce342633a98976ee2cd8ed75531
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-05-04 14:45:07 +0300

    all with market price

--- generators/generator_products.cpp
+++ generators/generator_products.cpp
@@ -14,5 +14,21 @@ void generate_products() {
         trade::Product("Artifacts", 0, 2, map::SpecialTypeCell::SANCTUARY),
         "data/json/products/artifacts.json"
     );
+    write_to_file(
+        trade::Product("Provision", 0, 2, map::SpecialTypeCell::SETTLEMENT),
+        "data/json/products/provision.json"
+    );
+    write_to_file(
+        trade::Product("Ammunition", 0, 2, map::SpecialTypeCell::FORTRESS),
+        "data/json/products/ammunition.json"
+    );
+    write_to_file(
+        trade::Product("Values", 2, 5, map::SpecialTypeCell::SETTLEMENT),
+        "data/json/products/values.json"
+    );
+    write_to_file(
+        trade::Product("Armament", 2, 5, map::SpecialTypeCell::FORTRESS),
+        "data/json/products/armament.json"
+    );
 }
 }  // namespace runebound::generator
