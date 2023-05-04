796d78e8129d7c4cb2770292f1d39c18186fddc7
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2023-05-04 20:20:13 +0300

    try2

--- include/network_client.hpp
+++ include/network_client.hpp
@@ -159,7 +159,6 @@ public:
         return m_game_client.m_last_dice_movement_result;
     };
 
-
     [[nodiscard]] character::StandardCharacter get_yourself_character() const {
         return m_character;
     }
