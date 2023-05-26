0c9c36bf5b142e43de90ab02726cfac5f35dec31
Artem Batygin <aa.batygin@gmail.com>
2023-05-26 18:14:22 +0300

    Fight names, hp, turn, pass button

--- src/graphics_fight_window.cpp
+++ src/graphics_fight_window.cpp
@@ -24,10 +24,16 @@ void Client::update_fight_window() {
         enemy_selected_tokens;
 
     if (m_network_client.is_game_need_update()) {
-        ::std::cout << "[[glog]] :: reset fight token" << ::std::endl;
         character_selected_tokens.clear();
         enemy_selected_tokens.clear();
     }
+
+    const auto &fight = m_network_client.get_game_client().m_fight_client;
+    int count = 0;
+    std::vector<::runebound::fight::TokenHandCount> your_tokens;
+    std::vector<::runebound::fight::TokenHandCount> opponent_tokens;
+    ::std::string your_name;
+    ::std::string opponent_name;
 
     {  // USE BUTTON
         if (m_network_client.get_yourself_character() != nullptr) {
@@ -42,16 +48,6 @@ void Client::update_fight_window() {
                 VerticalButtonTextureAlign::CENTER, 0, 0, texture,
                 [this, &char_vec = character_selected_tokens,
                  &enemy_vec = enemy_selected_tokens]() {
-                    std::cout
-                        << "Char use: " << char_vec.size()
-                        << " Enemy use: " << enemy_vec.size() << " total char: "
-                        << m_network_client.get_game_client()
-                               .m_fight_client.m_character_remaining_tokens
-                               .size()
-                        << " total enemy: "
-                        << m_network_client.get_game_client()
-                               .m_fight_client.m_enemy_remaining_tokens.size()
-                        << std::endl;
                     if (m_network_client.get_yourself_character()->get_state(
                         ) == ::runebound::character::StateCharacter::FIGHT) {
                         m_network_client.fight_make(
@@ -64,10 +60,10 @@ void Client::update_fight_window() {
                             ::runebound::character::StateCharacter::ENEMY) {
                             m_network_client.fight_make(
                                 ::runebound::fight::Participant::ENEMY,
-                                enemy_vec, char_vec
+                                char_vec, enemy_vec
                             );
                         } else {
-                            ::std::cout << "Invalid State\n" << ::std::endl;
+                            ::std::cout << "Invalid State!!!" << ::std::endl;
                         }
                     }
                 },
@@ -75,16 +71,183 @@ void Client::update_fight_window() {
             );
             win->add_button(
                 "use", button,
-                {win->width() - button.width() - 5, win->height() - 35}, true,
-                true
+                {win->width() - button.width() - 5,
+                 win->height() - 138 - button.height() - 5},
+                true, true
             );
         }
     }  // USE BUTTON
 
-    const auto &fight = m_network_client.get_game_client().m_fight_client;
-    int count = 0;
-    for (const auto &token :
-         fight.m_character_remaining_tokens) {  // CHARACTER TOKENS
+    {  // PASS BUTTON
+        if (m_network_client.get_yourself_character() != nullptr) {
+            Texture texture;
+            Button button;
+            texture.load_text_from_string(
+                m_graphic_renderer, m_fonts["FreeMono30"], "Pass",
+                {0x00, 0x00, 0x00, 0xFF}
+            );
+            button = Button(
+                200, 30, HorizontalButtonTextureAlign::CENTER,
+                VerticalButtonTextureAlign::CENTER, 0, 0, texture,
+                [this]() {
+                    if (m_network_client.get_yourself_character()->get_state(
+                        ) == ::runebound::character::StateCharacter::FIGHT) {
+                        m_network_client.fight_pass(
+                            ::runebound::fight::Participant::CHARACTER
+                        );
+                    } else {
+                        m_network_client.fight_pass(
+                            ::runebound::fight::Participant::ENEMY
+                        );
+                    }
+                },
+                []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
+            );
+            win->add_button(
+                "pass", button,
+                {win->width() - button.width() - 5,
+                 win->height() - 138 - button.height() - 5 - 35},
+                true, true
+            );
+        }
+    }  // PASS BUTTON
+
+    bool am_i_char;
+    ::runebound::character::Character opponent;
+
+    {  // GET YOURSELF AND OPPONENT
+        if (m_network_client.get_yourself_character() != nullptr &&
+            m_network_client.get_yourself_character()->get_state() ==
+                ::runebound::character::StateCharacter::FIGHT) {
+            am_i_char = true;
+            your_tokens = fight.m_character_remaining_tokens;
+            opponent_tokens = fight.m_enemy_remaining_tokens;
+            your_name = m_network_client.get_yourself_character()->get_name();
+            opponent_name = fight.m_enemy.get_name();
+        } else {
+            am_i_char = false;
+            opponent_tokens = fight.m_character_remaining_tokens;
+            your_tokens = fight.m_enemy_remaining_tokens;
+            your_name = fight.m_enemy.get_name();
+            for (const auto &op : m_network_client.m_game_client.m_characters) {
+                if (op.get_state() ==
+                    ::runebound::character::StateCharacter::FIGHT) {
+                    opponent = op;
+                    opponent_name = op.get_name();
+                    break;
+                }
+            }
+        }
+    }  // GET YOURSELF AND OPPONENT
+
+    {  // TURN
+        Texture texture;
+        ::std::string turn_name;
+        if (fight.get_turn() == ::runebound::fight::Participant::CHARACTER) {
+            if (am_i_char) {
+                turn_name = your_name;
+            } else {
+                turn_name = opponent_name;
+            }
+        } else {
+            if (am_i_char) {
+                turn_name = opponent_name;
+            } else {
+                turn_name = your_name;
+            }
+        }
+        texture.load_text_from_string(
+            m_graphic_renderer, m_fonts["FreeMono30"], "Turn: " + turn_name,
+            {0x00, 0x00, 0x00, 0xFF}
+        );
+        win->add_texture(
+            "turn", texture,
+            {win->width() - texture.width() - 5,
+             win->height() - 138 - texture.height() - 5 - 2 * 35},
+            true
+        );
+    }  // TURN
+
+    {  // YOUR NAME
+        Texture texture;
+        texture.load_text_from_string(
+            m_graphic_renderer, m_fonts["FreeMono30"], your_name,
+            {0x00, 0x00, 0x00, 0xFF}
+        );
+        win->add_texture("char_name", texture, {5, 138}, true);
+    }  // YOUR NAME
+
+    {  // YOUR HEART
+        SDL_Texture *tex = SDL_CreateTexture(
+            m_graphic_renderer, SDL_PIXELFORMAT_RGBA8888,
+            SDL_TEXTUREACCESS_TARGET, 20, 20
+        );
+        SDL_SetRenderTarget(m_graphic_renderer, tex);
+        SDL_SetRenderDrawColor(m_graphic_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(m_graphic_renderer);
+        SDL_SetRenderTarget(m_graphic_renderer, nullptr);
+        m_images["heart20"].render_to_texture(m_graphic_renderer, 0, 0, tex);
+        Texture texture = Texture(tex);
+        win->add_texture("char_heart", texture, {5, 173}, true);
+        ::std::string health;
+        if (am_i_char) {
+            health = ::std::to_string(
+                m_network_client.get_yourself_character()->get_health()
+            );
+        } else {
+            health = ::std::to_string(fight.m_enemy.get_health());
+        }
+        texture.load_text_from_string(
+            m_graphic_renderer, m_fonts["FreeMono20"], health,
+            {0x00, 0x00, 0x00, 0xFF}
+        );
+        win->add_texture("char_health", texture, {30, 173}, true);
+    }  // YOUR HEART
+
+    {  // ENEMY HEART
+        SDL_Texture *tex = SDL_CreateTexture(
+            m_graphic_renderer, SDL_PIXELFORMAT_RGBA8888,
+            SDL_TEXTUREACCESS_TARGET, 20, 20
+        );
+        SDL_SetRenderTarget(m_graphic_renderer, tex);
+        SDL_SetRenderDrawColor(m_graphic_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(m_graphic_renderer);
+        SDL_SetRenderTarget(m_graphic_renderer, nullptr);
+        m_images["heart20"].render_to_texture(m_graphic_renderer, 0, 0, tex);
+        Texture texture = Texture(tex);
+        win->add_texture(
+            "enemy_heart", texture,
+            {5, win->height() - 138 - texture.height() - 5 - 35}, true
+        );
+        ::std::string health;
+        if (am_i_char) {
+            health = ::std::to_string(fight.m_enemy.get_health());
+        } else {
+            health = ::std::to_string(opponent.get_health());
+        }
+        texture.load_text_from_string(
+            m_graphic_renderer, m_fonts["FreeMono20"], health,
+            {0x00, 0x00, 0x00, 0xFF}
+        );
+        win->add_texture(
+            "enemy_heath", texture,
+            {30, win->height() - 138 - texture.height() - 5 - 35}, true
+        );
+    }  // ENEMY HEART
+
+    {  // ENEMY NAME
+        Texture texture;
+        texture.load_text_from_string(
+            m_graphic_renderer, m_fonts["FreeMono30"], opponent_name,
+            {0x00, 0x00, 0x00, 0xFF}
+        );
+        win->add_texture(
+            "op_name", texture, {5, win->height() - 138 - texture.height() - 5},
+            true
+        );
+    }  // ENEMY NAME
+
+    for (const auto &token : your_tokens) {  // YOUR TOKENS
         SDL_Texture *tex = SDL_CreateTexture(
             m_graphic_renderer, SDL_PIXELFORMAT_RGBA8888,
             SDL_TEXTUREACCESS_TARGET, 128, 128
@@ -141,7 +304,7 @@ void Client::update_fight_window() {
                     case fight::HandFightTokens::NOTHING:
                         name = "none";
                         break;
-                };
+                }
                 if (init) {
                     name += "_init";
                 }
@@ -185,7 +348,7 @@ void Client::update_fight_window() {
                     case fight::HandFightTokens::NOTHING:
                         name = "none";
                         break;
-                };
+                }
                 if (init) {
                     name += "_init";
                 }
@@ -213,10 +376,10 @@ void Client::update_fight_window() {
             true, true
         );
         ++count;
-    }  // CHARACTER TOKENS
+    }  // YOUR TOKENS
 
     count = 0;
-    for (const auto &token : fight.m_enemy_remaining_tokens) {  // ENEMY TOKENS
+    for (const auto &token : opponent_tokens) {  // ENEMY TOKENS
         SDL_Texture *tex = SDL_CreateTexture(
             m_graphic_renderer, SDL_PIXELFORMAT_RGBA8888,
             SDL_TEXTUREACCESS_TARGET, 128, 128
@@ -225,7 +388,7 @@ void Client::update_fight_window() {
         SDL_SetRenderDrawColor(m_graphic_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(m_graphic_renderer);
         SDL_SetRenderTarget(m_graphic_renderer, nullptr);
-        {      // REDNER
+        {      // RENDER
             {  // BACKGROUND
                 if (::std::find(
                         enemy_selected_tokens.begin(),
@@ -273,7 +436,7 @@ void Client::update_fight_window() {
                     case fight::HandFightTokens::NOTHING:
                         name = "none";
                         break;
-                };
+                }
                 if (init) {
                     name += "_init";
                 }
@@ -317,7 +480,7 @@ void Client::update_fight_window() {
                     case fight::HandFightTokens::NOTHING:
                         name = "none";
                         break;
-                };
+                }
                 if (init) {
                     name += "_init";
                 }
