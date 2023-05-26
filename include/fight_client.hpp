1d577e37c218abbe8fa880f53c59b5949acc422e
BogruAKVD <bnmmnb456654@inbox.ru>
2023-05-26 19:38:40 +0300

    add get_winner in fight_client

--- include/fight_client.hpp
+++ include/fight_client.hpp
@@ -1,52 +1,65 @@
 
 #ifndef RUNEBOUND_FIGHT_CLIENT_HPP
 #define RUNEBOUND_FIGHT_CLIENT_HPP
+
 #include "fight.hpp"
 
 namespace runebound::fight {
-struct FightClient {
-    friend struct Character;
+    struct FightClient {
+        friend struct Character;
 
-    Participant m_turn = Participant::CHARACTER;
-    bool m_pass_character = false;
-    bool m_pass_enemy = false;
-    ::runebound::character::Character m_character;
-    Enemy m_enemy;
-    std::vector<TokenHandCount> m_enemy_remaining_tokens;
-    std::vector<TokenHandCount> m_character_remaining_tokens;
-    FightClient() = default;
+        Participant m_turn = Participant::CHARACTER;
+        bool m_pass_character = false;
+        bool m_pass_enemy = false;
+        ::runebound::character::Character m_character;
+        Enemy m_enemy;
+        std::vector<TokenHandCount> m_enemy_remaining_tokens;
+        std::vector<TokenHandCount> m_character_remaining_tokens;
 
-    FightClient(Fight &fight) {
-        m_turn = fight.m_turn;
-        m_pass_character = fight.m_pass_character;
-        m_pass_enemy = fight.m_pass_enemy;
-        m_character = *(fight.m_character);
-        m_enemy = *(fight.get_enemy());
-        m_enemy_remaining_tokens = fight.m_enemy_remaining_tokens;
-        m_character_remaining_tokens = fight.m_character_remaining_tokens;
-    }
+        FightClient() = default;
 
-    bool check_end_fight() const {
-        return m_character.get_health() == 0 || m_enemy.get_health() == 0;
-    }
+        FightClient(Fight &fight) {
+            m_turn = fight.m_turn;
+            m_pass_character = fight.m_pass_character;
+            m_pass_enemy = fight.m_pass_enemy;
+            m_character = *(fight.m_character);
+            m_enemy = *(fight.get_enemy());
+            m_enemy_remaining_tokens = fight.m_enemy_remaining_tokens;
+            m_character_remaining_tokens = fight.m_character_remaining_tokens;
+        }
 
-    [[nodiscard]] Participant get_turn() const {
-        return m_turn;
-    }
+        bool check_end_fight() const {
+            return m_character.get_health() == 0 || m_enemy.get_health() == 0;
+        }
 
-    [[nodiscard]] std::vector<TokenHandCount> get_character_remaining_tokens(
-    ) const {
-        return m_character_remaining_tokens;
-    }
+        [[nodiscard]] Participant get_winner() const {
+            if (!check_end_fight()) {
+                throw std::runtime_error("Fight is still going");
+            }
+            if (m_enemy.get_health() == 0) {
+                return Participant::CHARACTER;
+            }
+            return Participant::ENEMY;
+        }
 
-    [[nodiscard]] std::vector<TokenHandCount> get_enemy_remaining_tokens(
-    ) const {
-        return m_enemy_remaining_tokens;
-    }
+        [[nodiscard]] Participant get_turn() const {
+            return m_turn;
+        }
 
-    friend void to_json(nlohmann::json &json, const FightClient &fight);
-    friend void from_json(const nlohmann::json &json, FightClient &fight);
-};
+        [[nodiscard]] std::vector<TokenHandCount> get_character_remaining_tokens(
+        ) const {
+            return m_character_remaining_tokens;
+        }
+
+        [[nodiscard]] std::vector<TokenHandCount> get_enemy_remaining_tokens(
+        ) const {
+            return m_enemy_remaining_tokens;
+        }
+
+        friend void to_json(nlohmann::json &json, const FightClient &fight);
+
+        friend void from_json(const nlohmann::json &json, FightClient &fight);
+    };
 
 }  // namespace runebound::fight
 
