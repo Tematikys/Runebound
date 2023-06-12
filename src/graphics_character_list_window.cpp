746a3ef982b563e9b1e5a00ad6d3bc5c962db2d1
Artem Batygin <aa.batygin@gmail.com>
2023-06-12 15:37:07 +0300

    Refactor character list

--- src/graphics_character_list_window.cpp
+++ src/graphics_character_list_window.cpp
@@ -5,108 +5,61 @@ void Client::init_character_list_window() {
     auto window = std::make_unique<Window>(
         Window(WINDOW_WIDTH, WINDOW_HEIGHT, {0xFF, 0xFF, 0xFF, 0xFF})
     );
-    Texture texture;
-    Button button;
-
-    // ===== MAIN MENU BUTTON =====
-    texture.load_text_from_string(
-        m_graphic_renderer, m_fonts["FreeMono30"], "Main menu",
-        {0x00, 0x00, 0x00, 0xFF}
-    );
-    button = Button(
-        10 * 18, 30, HorizontalButtonTextureAlign::CENTER,
-        VerticalButtonTextureAlign::CENTER, 0, 0, texture,
-        [this]() {
-            m_network_client.exit_game();
-            m_window.set_updatability_window("char_list", false);
-            m_window.set_visibility_window("char_list", false);
-            m_window.set_active_window("main_menu");
-            m_window.set_updatability_window("main_menu", true);
-            m_window.set_visibility_window("main_menu", true);
-        },
-        []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
-    );
-    window->add_button(
-        "main_menu", button,
-        {WINDOW_WIDTH - 10 * 18 - 5, WINDOW_HEIGHT - 35 * 2}, true, true
-    );
-    // ===== MAIN MENU BUTTON =====
-
-    // ===== EXIT BUTTON =====
-    texture.load_text_from_string(
-        m_graphic_renderer, m_fonts["FreeMono30"], "Exit",
-        {0x00, 0x00, 0x00, 0xFF}
-    );
-    button = Button(
-        10 * 18, 30, HorizontalButtonTextureAlign::CENTER,
-        VerticalButtonTextureAlign::CENTER, 0, 0, texture,
-        [this]() {
-            m_network_client.exit_game();
-            m_is_running = false;
-        },
-        []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
-    );
-    window->add_button(
-        "exit", button, {WINDOW_WIDTH - 10 * 18 - 5, WINDOW_HEIGHT - 30 - 5},
-        true, true
-    );
-    // ===== EXIT BUTTON =====
-
     m_window.add_window("char_list", std::move(window), {0, 0}, false, false);
 }
 
 void Client::update_character_list_window() {
     auto *win = m_window.get_window("char_list");
-    win->remove_all_buttons();
-
     Texture texture;
     Button button;
 
-    // ===== MAIN MENU BUTTON =====
-    texture.load_text_from_string(
-        m_graphic_renderer, m_fonts["FreeMono30"], "Main menu",
-        {0x00, 0x00, 0x00, 0xFF}
-    );
-    button = Button(
-        10 * 18, 30, HorizontalButtonTextureAlign::CENTER,
-        VerticalButtonTextureAlign::CENTER, 0, 0, texture,
-        [this]() {
-            m_network_client.exit_game();
-            m_window.set_updatability_window("char_list", false);
-            m_window.set_visibility_window("char_list", false);
-            m_window.set_active_window("main_menu");
-            m_window.set_updatability_window("main_menu", true);
-            m_window.set_visibility_window("main_menu", true);
-        },
-        []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
-    );
-    win->add_button(
-        "main_menu", button,
-        {WINDOW_WIDTH - 10 * 18 - 5, WINDOW_HEIGHT - 35 * 2}, true, true
-    );
-    // ===== MAIN MENU BUTTON =====
+    win->remove_all_buttons();
 
-    // ===== EXIT BUTTON =====
-    texture.load_text_from_string(
-        m_graphic_renderer, m_fonts["FreeMono30"], "Exit",
-        {0x00, 0x00, 0x00, 0xFF}
-    );
-    button = Button(
-        10 * 18, 30, HorizontalButtonTextureAlign::CENTER,
-        VerticalButtonTextureAlign::CENTER, 0, 0, texture,
-        [this]() {
-            m_network_client.exit_game();
-            m_is_running = false;
-        },
-        []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
-    );
-    win->add_button(
-        "exit", button, {WINDOW_WIDTH - 10 * 18 - 5, WINDOW_HEIGHT - 30 - 5},
-        true, true
-    );
-    // ===== EXIT BUTTON =====
+    {  //  MAIN MENU BUTTON
+        texture.load_text_from_string(
+            m_graphic_renderer, m_fonts["FreeMono30"], "Main menu",
+            {0x00, 0x00, 0x00, 0xFF}
+        );
+        button = Button(
+            10 * 18, 30, HorizontalButtonTextureAlign::CENTER,
+            VerticalButtonTextureAlign::CENTER, 0, 0, texture,
+            [this]() {
+                m_network_client.exit_game();
+                m_window.set_updatability_window("char_list", false);
+                m_window.set_visibility_window("char_list", false);
+                m_window.set_active_window("main_menu");
+                m_window.set_updatability_window("main_menu", true);
+                m_window.set_visibility_window("main_menu", true);
+            },
+            []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
+        );
+        win->add_button(
+            "main_menu", button,
+            {WINDOW_WIDTH - 10 * 18 - 5, WINDOW_HEIGHT - 35 * 2}, true, true
+        );
+    }  //  MAIN MENU BUTTON
 
-    int i = 0;
+    {  //  EXIT BUTTON
+        texture.load_text_from_string(
+            m_graphic_renderer, m_fonts["FreeMono30"], "Exit",
+            {0x00, 0x00, 0x00, 0xFF}
+        );
+        button = Button(
+            10 * 18, 30, HorizontalButtonTextureAlign::CENTER,
+            VerticalButtonTextureAlign::CENTER, 0, 0, texture,
+            [this]() {
+                m_network_client.exit_game();
+                m_is_running = false;
+            },
+            []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
+        );
+        win->add_button(
+            "exit", button,
+            {WINDOW_WIDTH - 10 * 18 - 5, WINDOW_HEIGHT - 30 - 5}, true, true
+        );
+    }  //  EXIT BUTTON
+
+    int count = 0;
     for (const auto &character :
          m_network_client.get_game_client().m_remaining_standard_characters) {
         std::string name;
@@ -129,6 +82,8 @@ void Client::update_character_list_window() {
             case character::StandardCharacter::MASTER_THORN:
                 name = "Master Thorn";
                 break;
+            case character::StandardCharacter::NONE:
+                break;
         }
         texture.load_text_from_string(
             m_graphic_renderer, m_fonts["FreeMono30"], name,
@@ -147,8 +102,8 @@ void Client::update_character_list_window() {
             },
             []() {}, {0xFF, 0xFF, 0xFF, 0xFF}, {0x00, 0x00, 0x00, 0xFF}
         );
-        win->add_button(name, button, {5, i * 35 + 5}, true, true);
-        ++i;
+        win->add_button(name, button, {5, count * 35 + 5}, true, true);
+        ++count;
     }
 }
 }  // namespace runebound::graphics
